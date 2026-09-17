6f97c29b6ed1a141ed66540839b7a145a0d2fa34
Noev Chanmakara <jrmakara97@gmail.com>
2026-09-17 16:30:09 +0700

    add NavBarComponent and integrate it into Home; update text in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import NavBarComponent from "@/components/NavBarComponent";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <NavBarComponent />
       <h1>Hello World</h1>
       <p>Welcome to mini project</p>
       <p>This is add the ssh into the jenkins file</p>
       <p>I love hrd</p>
       <p>I love devops</p>
       <p>I love AI LLM</p>
+      <p>Siengly love his gf</p>
     </div>
   );
 }
---
+++ src/components/NavBarComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBarComponent() {
+  return (
+    <div className="flex flex-row justify-between bg-purple-600 ">
+      <p>Homepage</p>
+      <p>Product</p>
+      <p>My Profil</p>
+    </div>
+  );
+}
